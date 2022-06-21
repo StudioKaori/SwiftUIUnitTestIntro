c6ce9fd871f05b4f0d104681bf66638c1f7337cd
Studio Kaori <studiokaori.com@gmail.com>
2022-06-21 09:58:01 +0200

    Initial Commit

---
+++ SwiftUIUnintTestIntro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIUnintTestIntro
+//
+//  Created by Kaori Persson on 2022-06-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIUnintTestIntro/SwiftUIUnintTestIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUnintTestIntroApp.swift
+//  SwiftUIUnintTestIntro
+//
+//  Created by Kaori Persson on 2022-06-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUnintTestIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
